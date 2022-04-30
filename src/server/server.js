c94ffe16132ad54ea1ae684f74bbc7bcf0cdd7b8
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-04-30 15:42:31 +0530

    changes db

--- src/server/server.js
+++ src/server/server.js
@@ -3,3 +3,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://pooga:12345@cluster0.qaybd.mongodb.net/visitorDB"
+);
+
+app.use("/", require("./route"));
+
+app.listen(3000, function () {
+  console.log("listening port 3000");
+});
